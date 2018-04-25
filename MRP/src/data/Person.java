a8e78c72def0f3475baa38ca0d50b9c2ed997b87
florianhanak-hammerl <florianhanak-hammerl@kk-gym-edv1-04.gym.kk.local>
2018-04-25 16:09:24 +0200

    Channnnnnggggesss

--- MRP/src/data/Person.java
+++ MRP/src/data/Person.java
@@ -1,5 +1,42 @@
 package data;
 
-public class Person {
+import java.util.ArrayList;
 
+public class Person {
+	//
+	private static ArrayList<Person> personenListe = new ArrayList<>();
+	//
+	private String vorname;
+	private String nachname;
+	protected boolean female;
+	
+	
+	//
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public boolean isFemale() {
+		return female;
+	}
+	public void setFemale(boolean female) {
+		this.female = female;
+	}
+
+	@Override
+	public String toString() {
+		return(isFemale() ? "Frau" : "Mann") + " " + getVorname() + " " + getNachname();
+	}
+	public static ArrayList<Person> getPersonenListe() {
+		return personenListe;
+	}
+	
 }
---
+++ MRP/src/prog/Prog.java
@@ -0,0 +1,60 @@
+package prog;
+
+import data.Person;
+import data.Student;
+
+public class Prog {
+
+	public static void main(String[] args) {
+		new Prog();
+
+	}
+	
+	public Prog() {
+		Person p1 = new Person();
+		p1.setVorname("Anna");
+		p1.setNachname("Meia");
+		p1.setFemale(true);
+		System.out.println(p1);
+		Person.getPersonenListe().add(p1);
+		//
+		Person p2 = new Person();
+		p2.setVorname("Heinrich");
+		p2.setNachname("Meia");
+		//p1.setFemale(true);
+		System.out.println(p2);
+		Person.getPersonenListe().add(p2);
+		//
+		Student s1 = new Student();
+		s1.setVorname("Paul");
+		s1.setNachname("Schring");
+		s1.setGrade(2018);
+		System.out.println(s1);
+		Person.getPersonenListe().add(s1);
+		//
+		//Student s2 = (Student) p2; geht nd aber
+		Person p3 = s1;
+		Person.getPersonenListe().add(p3);
+		//
+		System.out.println("-----");
+		System.out.println(p1 instanceof Person);
+		System.out.println(p1 instanceof Student);
+		System.out.println("-----");
+		System.out.println(s1 instanceof Person);
+		System.out.println(s1 instanceof Student);
+		System.out.println("-----");
+		System.out.println(p3 instanceof Person);
+		System.out.println(p3 instanceof Student);
+		System.out.println("-----");
+		//
+		for(Person p : Person.getPersonenListe()) {
+			System.out.println(p.getVorname() + " " + p.getNachname());
+			if(p instanceof Student) {
+				Student s = (Student) p;
+				System.out.println("Im Jahrgang"  + s.getGrade());
+			}
+		}
+			
+	}
+
+}
